b91bd2ad7acba0cd8cd09bac6d2f144af304228b
ahmed sulaiman <asulaiman@appzonegroup.com>
2020-03-10 00:10:18 +0100

    Added Extra Functionalities

--- InstitutionsAPI/Controllers/StudentsController.cs
+++ InstitutionsAPI/Controllers/StudentsController.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using InstitutionsAPI.Contexts;
 using InstitutionsAPI.Models;
+using InstitutionsAPI.Utilities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using InstitutionsAPI.Extensions;
 
 namespace InstitutionsAPI.Controllers
 {
@@ -19,15 +21,129 @@ namespace InstitutionsAPI.Controllers
         {
         }
 
-        // GET: api/Institutions
+        // GET: api/Institutions/{institutionCode}
         [HttpGet("{institutionCode}")]
         public IEnumerable<Student> GetStudents([FromRoute] string institutionCode)
         {
-            // get institution's connection string.
-            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
-            return new List<Student>();
-            // check appropriate database
+            IList<Student> students = new List<Student>();
 
+            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+
+            if (connectionString == null || string.IsNullOrEmpty(connectionString))
+                return students;
+
+            var studentsData = DatabaseHelper.ExecuteQuery(connectionString, "Select * from [dbo].[Students]");
+
+            return studentsData.Select(s => s.ToObject<Student>());
+          
+        }
+
+        // GET: api/Institutions/5
+        [HttpGet("{institutionCode}/{id}")]
+        public async Task<IActionResult> GetStudent([FromRoute] string institutionCode, [FromRoute]int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+            if (connectionString == null || string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("Invalid Institution Code");
+            }
+
+            var studentExpando = DatabaseHelper.ExecuteFindQuery(connectionString, $"Select * from [dbo].[Students] where ID={id}");
+
+            var student = studentExpando.ToObject<Student>();
+
+            if (student == null || student.Name == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Institutions/5
+        [HttpPut("{institutionCode}/{id}")]
+        public async Task<IActionResult> PutStudent([FromRoute] string institutionCode, [FromRoute] int id, [FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+            if (connectionString == null || string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("Invalid Institution Code");
+            }
+
+            if (id != student.ID)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                DatabaseHelper.ExecutePureQuery(connectionString, $@"Update [dbo].[Students] set [Name] = '{student.Name}' where ID = {id}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Institutions
+        [HttpPost("{institutionCode}")]
+        public async Task<IActionResult> PostStudent([FromRoute] string institutionCode, [FromBody] Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+            if (connectionString == null || string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("Invalid Institution Code");
+            }
+
+            try
+            {
+                DatabaseHelper.ExecutePureQuery(connectionString, $@"INSERT INTO [dbo].[Students]
+                                                                    ([Name]) VALUES ('{student.Name}')");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.ToString());
+            }
+
+            return CreatedAtAction("GetStudent", new { id = student.ID }, student);
+        }
+
+        // DELETE: api/Institutions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var institution = await _context.Institutions.FindAsync(id);
+            if (institution == null)
+            {
+                return NotFound();
+            }
+
+            _context.Institutions.Remove(institution);
+            await _context.SaveChangesAsync();
+
+            return Ok(institution);
         }
     }
 }
