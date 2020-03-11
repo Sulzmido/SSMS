d855cb6be6a2dc037376651a21349897c0c094b8
ahmed sulaiman <asulaiman@appzonegroup.com>
2020-03-11 09:10:16 +0100

    Complete Refactoring of Subject and Subject Categories Controllers

--- InstitutionsAPI/Controllers/SubjectsController.cs
+++ InstitutionsAPI/Controllers/SubjectsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InstitutionsAPI.Contexts;
+using InstitutionsAPI.DAL;
 using InstitutionsAPI.Models;
 using InstitutionsAPI.Utilities;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,7 @@ namespace InstitutionsAPI.Controllers
     public class SubjectsController : ControllerBase
     {
         private readonly ApplicationContext _context = new ApplicationContext();
-        private readonly string _tableName = "Subjects";
+        private readonly SubjectDAO _dao = new SubjectDAO();
 
         public SubjectsController()
         {
@@ -28,24 +29,25 @@ namespace InstitutionsAPI.Controllers
         [HttpGet("{institutionCode}")]
         public IEnumerable<Subject> GetSubjects([FromRoute] string institutionCode)
         {
-            IList<Subject> subjects = new List<Subject>();
+            IEnumerable<Subject> subjects = new List<Subject>();
 
             var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
 
             if (connectionString == null || string.IsNullOrEmpty(connectionString))
                 return subjects;
+
+            _dao.ConnectionString = connectionString;
+
             try
             {
-                var subjectsData = DatabaseHelper.ExecuteSelectQuery(connectionString, $"Select * from [dbo].[{_tableName}]");
-
-                return subjectsData.Select(s => SerializationHelper.UnboxEntity<Subject>(s));
+                subjects = _dao.GetAll();
+                return subjects;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
                 return subjects;
             }
-
         }
 
         // GET: api/Subjects/{institutionCode}/5
@@ -58,27 +60,23 @@ namespace InstitutionsAPI.Controllers
             }
 
             var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+
             if (connectionString == null || string.IsNullOrEmpty(connectionString))
             {
                 return BadRequest("Invalid Institution Code");
             }
 
-            IDictionary<string, object> subjectData = new Dictionary<string, object>();
+            _dao.ConnectionString = connectionString;
+
             try
             {
-                subjectData = await DatabaseHelper.ExecuteSelectFindQueryAsync(connectionString, $"Select * from [dbo].[{_tableName}] where ID={id}");
-
-                if (subjectData.Count < 1)
-                {
-                    return NotFound();
-                }
-
-                var subject = SerializationHelper.UnboxEntity<Subject>(subjectData);
+                var subject = await _dao.FindAsync(id);
 
                 if (subject == null)
                 {
                     return NotFound();
                 }
+
                 return Ok(subject);
             }
             catch (Exception ex)
@@ -97,37 +95,23 @@ namespace InstitutionsAPI.Controllers
                 return BadRequest(ModelState);
             }
 
-            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
-            if (connectionString == null || string.IsNullOrEmpty(connectionString))
-            {
-                return BadRequest("Invalid Institution Code");
-            }
-
             if (id != subject.ID)
             {
                 return BadRequest();
             }
 
-            // edit will be a little tricky  
-            // a retrieval will happen first
-            // we iterate through the values provided in the Subject parameter.
+            var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+
+            if (connectionString == null || string.IsNullOrEmpty(connectionString))
+            {
+                return BadRequest("Invalid Institution Code");
+            }
+
+            _dao.ConnectionString = connectionString;
 
             try
             {
-                if (subject.Category.GetType() == typeof(string))
-                {
-                    subject.Category = Convert.ToString(Convert.ToInt32(subject.Category));
-                }
-                else
-                {
-                    var category = ((JObject)subject.Category).ToObject<SubjectCategory>();
-                    subject.Category = Convert.ToString(category.ID);
-                }
-
-                var serializerSettings = SerializationHelper.GetIgnoreIDSerializerSetting(typeof(Subject));
-                var serializedEntity = JsonConvert.SerializeObject(subject, serializerSettings);
-
-                await DatabaseHelper.ExecuteQueryAsync(connectionString, $@"Update [dbo].[{_tableName}] set [SerializedEntity] = '{serializedEntity}' where ID = {id}");
+                await _dao.UpdateAsync(subject);
             }
             catch (Exception ex)
             {
@@ -148,39 +132,21 @@ namespace InstitutionsAPI.Controllers
             }
 
             var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+
             if (connectionString == null || string.IsNullOrEmpty(connectionString))
             {
                 return BadRequest("Invalid Institution Code");
             }
 
+            _dao.ConnectionString = connectionString;
+
             try
             {
-                var tableExists = DatabaseHelper.ExecuteTableCheck(connectionString, _tableName);
-
-                if (!tableExists)
-                {
-                    DatabaseHelper.CreateTable(connectionString, _tableName);
-                }
-
-                // this part of the code can be astracted away
-                if (subject.Category.GetType() == typeof(string))
-                {
-                    subject.Category = Convert.ToString(Convert.ToInt32(subject.Category));
-                }
-                else
-                {
-                    var category = ((JObject)subject.Category).ToObject<SubjectCategory>();
-                    subject.Category = Convert.ToString(category.ID);
-                } 
-
-                var serializerSettings = SerializationHelper.GetIgnoreIDSerializerSetting(typeof(Subject));
-                var serializedEntity = JsonConvert.SerializeObject(subject, serializerSettings);
-
-                subject.ID = await DatabaseHelper.ExecuteInsertQueryAsync(connectionString, $@"INSERT INTO [dbo].[{_tableName}]
-                                                                    ([SerializedEntity]) output INSERTED.ID VALUES ('{serializedEntity}')");
+                subject = await _dao.InsertAsync(subject);
             }
             catch (Exception ex)
             {
+                // Log Exception.
                 return BadRequest(ex.ToString());
             }
 
@@ -197,26 +163,28 @@ namespace InstitutionsAPI.Controllers
             }
 
             var connectionString = _context.Institutions.Single(i => i.Code.Equals(institutionCode)).ConnectionString;
+
             if (connectionString == null || string.IsNullOrEmpty(connectionString))
             {
                 return BadRequest("Invalid Institution Code");
             }
 
-            var subjectData = await DatabaseHelper.ExecuteSelectFindQueryAsync(connectionString, $"Select * from [dbo].[{_tableName}] where ID={id}");
-            // find a better way of figuring out if the data exists in the database
-            var subject = SerializationHelper.UnboxEntity<Subject>(subjectData);
-            
-            if (subject == null || subject.Name == null)
+            _dao.ConnectionString = connectionString;
+
+            var subject = await _dao.FindAsync(id);
+
+            if (subject == null)
             {
                 return NotFound();
             }
 
             try
             {
-                await DatabaseHelper.ExecuteQueryAsync(connectionString, $@"DELETE FROM [dbo].[{_tableName}] WHERE ID='{subject.ID}'");
+                await _dao.DeleteAsync(subject);
             }
             catch (Exception ex)
             {
+                // Log exception.
                 return BadRequest(ex.ToString());
             }
 
