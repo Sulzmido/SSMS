8ebdd7d7b129bf8ebbc2e253ec4bb22504de90d8
ahmed sulaiman <asulaiman@appzonegroup.com>
2020-03-11 23:30:38 +0100

    Student,Subject,StudentCategory API's complete.

--- InstitutionsAPI/Context/DynamicContext.cs
+++ InstitutionsAPI/Context/DynamicContext.cs
@@ -9,7 +9,9 @@ namespace InstitutionsAPI.Contexts
     public class DynamicContext : DbContext
     {
         // What is the cost of creating a context for every request ??
+        // Check if connection string can be changed on an already initialized context.
         //  code simplicity vs speed.
+
         private readonly string _connectionString;
         public DynamicContext(string connectionString)
         {
--- InstitutionsAPI/DAL/StudentDAO.cs
+++ InstitutionsAPI/DAL/StudentDAO.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace InstitutionsAPI.DAL
@@ -14,6 +15,7 @@ namespace InstitutionsAPI.DAL
 
         public new async Task<Student> InsertAsync(Student student)
         {
+            // POST : Validate all enums, extract id from sub-entities.
             string sex = student.Sex;
 
             if(sex != null && !sex.Equals(Gender.Male.ToString()) && !sex.Equals(Gender.Female.ToString()))
@@ -46,7 +48,63 @@ namespace InstitutionsAPI.DAL
 
         public new async Task UpdateAsync(Student student)
         {
-            await base.UpdateAsync(student);
+            // Enum Validation , extract id from sub-entities.
+            string sex = student.Sex;
+
+            if (sex != null && !sex.Equals(Gender.Male.ToString()) && !sex.Equals(Gender.Female.ToString()))
+            {
+                throw new Exception("Invalid Sex Value");
+            }
+
+            string status = student.Parent?.MarriageStatus;
+
+            if (status != null && !status.Equals(Status.Married.ToString()) && !status.Equals(Status.Seperated.ToString()))
+            {
+                throw new Exception("Invalid MarriageStatus Value");
+            }
+
+            var entityToUpdate = await FindAsync(student.ID);
+
+            PropertyInfo[] props = typeof(Student).GetProperties();
+
+            foreach(PropertyInfo prop in props)
+            {
+                if(prop.Name == "ID")
+                {
+                    continue;
+                }
+
+                // Handling Sub-Objects
+                if(prop.Name == "Parent")
+                {
+                    var value = prop.GetValue(student);
+
+                    if(value != null)
+                    {
+                        foreach (PropertyInfo property in typeof(Parent).GetProperties())
+                        {
+                            var parentPropertyValue = property.GetValue(student.Parent);
+
+                            if (parentPropertyValue != null)
+                            {
+                                property.SetValue(entityToUpdate.Parent, parentPropertyValue);
+                            }
+                        }
+                    } 
+                }
+
+                else
+                {
+                    var value = prop.GetValue(student);
+
+                    if (value != null)
+                    {
+                        prop.SetValue(entityToUpdate, value);
+                    }
+                }                    
+            }            
+
+            await base.UpdateAsync(entityToUpdate);
         }
 
         public new async Task DeleteAsync(Student student)
