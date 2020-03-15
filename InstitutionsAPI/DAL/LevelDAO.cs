a1e1bfd218f3e9c67974a644ac92cdbdf4fde135
Jayora50 <umorenjoshua50@gmail.com>
2020-03-15 09:06:55 +0100

    Worked on the Data Access layer for levels, the Insert, find and Update Async functions.

---
+++ InstitutionsAPI/DAL/LevelDAO.cs
@@ -0,0 +1,53 @@
+﻿using InstitutionsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace InstitutionsAPI.DAL
+{
+    public class LevelDAO : BaseDAO<Level>
+    {
+        public LevelDAO(string tableName = "Level") : base(tableName)
+        {
+        }
+        public new async Task<Level> InsertAsync(Level level)
+        {
+            var result = await base.InsertAsync(level);
+            return result;
+        }
+        public new async Task<Level> FindAsync(int ID)
+        {
+            var result = await base.FindAsync(ID);
+            return result;
+        }
+        public new IEnumerable<Level> GetAll()
+        {
+            var result = base.GetAll();
+            return result;
+        }
+        public new async Task UpdateAsync(Level level)
+        {
+            var entityToUpdate = await FindAsync(level.ID);
+
+            PropertyInfo[] props = typeof(Level).GetProperties();
+
+            foreach (PropertyInfo prop in props)
+            {
+                if (prop.Name == "ID")
+                {
+                    continue;
+                }
+                var value = prop.GetValue(level);
+
+                if (value != null)
+                {
+                    prop.SetValue(entityToUpdate, value);
+                }
+
+            }
+            await base.UpdateAsync(level);
+        }
+    }
+}
